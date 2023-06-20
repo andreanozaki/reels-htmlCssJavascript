75bf3e2cfae9d5271df92194bcea648e7e23208b
Andrea <nozakiandrea@gmail.com>
2023-06-20 18:27:44 -0300

    add event.key

--- modal-ui-js/script.js
+++ modal-ui-js/script.js
@@ -5,24 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-
-
-   
     const openModal = function(){
-        console.log('Button clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
     };
-
 
     const closeModal = function (){
         modal.classList.add('hidden');
         overlay.classList.add('hidden');
     };
 
-
     for (let i=0; i < btnsOpenModal.length;i++ )
     btnsOpenModal[i].addEventListener ('click',openModal);
 
     btnCloseModal.addEventListener('click',closeModal);
     overlay.addEventListener('click',closeModal);
+
+    document.addEventListener('keydown', function(event){
+        console.log(event.key);
+   // se a tecla pressionada for escape e modal nao conter a class hidden - function closeModal      
+        if (event.key ==='Escape'&& !modal.classList.contains('hidden')){
+                closeModal();
+            }
+    });
